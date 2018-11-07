26430d2fd42dc2a7464881c901b5d72ebcebe95c
Tom Coleman <tom@percolatestudio.com>
2018-11-07 15:15:56 +1100

    Use styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import styled from 'styled-components'
+
+const Background = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Centerer = styled.div`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Title = styled.h1`
+  margin: 0;
+`
+
+const TitleLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
+
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <Background>
+    <Centerer>
+      <Title>
+        <TitleLink to="/">{siteTitle}</TitleLink>
+      </Title>
+    </Centerer>
+  </Background>
 )
 
 export default Header
