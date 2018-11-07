454eb16b7c8818f337246b1ecb6b8535eee00dec
Tom Coleman <tom@percolatestudio.com>
2018-11-07 15:22:08 +1100

    Added chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,12 @@
 import { configure } from '@storybook/react'
+import 'storybook-chromatic'
 
 // Needed to add these to get Gatsby's links working.
 // We may need to do more: https://www.gatsbyjs.org/docs/unit-testing/
 window.__PATH_PREFIX__ = '/'
-window.__loader = () => null
-
+window.___loader = {
+  enqueue: () => null,
+}
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/)
 function loadStories() {
