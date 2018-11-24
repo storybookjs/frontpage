f36f75fcf44fcf6d2226d4e51a7b2e8188f7de11
domyen <dom@hichroma.com>
2018-11-23 23:42:40 -0500

    TODO: add global styles @tmeasday

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,16 +1,19 @@
-import { configure } from '@storybook/react'
-import 'storybook-chromatic'
+import { configure } from '@storybook/react';
+import 'storybook-chromatic';
+
+// add styled-components global styles here
 
 // Needed to add these to get Gatsby's links working.
 // We may need to do more: https://www.gatsbyjs.org/docs/unit-testing/
-window.__PATH_PREFIX__ = '/'
+window.__PATH_PREFIX__ = '/';
 window.___loader = {
   enqueue: () => null,
-}
+};
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stories.js$/)
+const req = require.context('../src', true, /.stories.js$/);
+
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './header';
-import './layout.css';
+// add global styles here
+
+import Header from './Header';
+// import Hero from './Hero';
+// import SocialProof from './SocialProof';
+// import SectionHero from './SectionHero';
+// import SectionBenefits from './SectionBenefits';
+// import Testimonial from './Testimonial';
+// import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -29,16 +36,8 @@ const Layout = ({ children }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+
+        {children}
       </>
     )}
   />
