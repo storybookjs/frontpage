9908906313afe6389dd9f0b07bb4423971175c18
domyen <dom@hichroma.com>
2018-11-26 09:39:04 -0500

    Some git gymnastics to make it recognize capitalized component names

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -8,12 +8,6 @@ const Background = styled.div`
   margin-bottom: 1.45rem;
 `;
 
-const Centerer = styled.div`
-  margin: 0 auto;
-  max-width: 960;
-  padding: 1.45rem 1.0875rem;
-`;
-
 const Title = styled.h1`
   margin: 0;
 `;
@@ -25,12 +19,10 @@ const TitleLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <Background>
-    <Centerer>
-      <Title>
-        <TitleLink to="/">{siteTitle}</TitleLink>
-        <img src={withPrefix('images/gatsby-icon.png')} />
-      </Title>
-    </Centerer>
+    <Title>
+      <TitleLink to="/">{siteTitle}</TitleLink>
+      <img src={withPrefix('images/gatsby-icon.png')} />
+    </Title>
   </Background>
 );
 
