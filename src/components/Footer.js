da5d6be75b8228af0b51ae209329d11e7b0b26e6
domyen <dom@hichroma.com>
2018-11-29 18:29:01 -0500

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
 import styled from 'styled-components';
 
 const Background = styled.div`
@@ -8,6 +6,6 @@ const Background = styled.div`
   margin-bottom: 1.45rem;
 `;
 
-const Footer = ({ siteTitle }) => <Background>Footer</Background>;
-
-export default Footer;
+export default function Footer({ ...props }) {
+  return <Background {...props}>Footer</Background>;
+}
---
+++ src/components/Footer.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Footer from './Footer';
+
+storiesOf('Layout/Footer', module).add('default', () => <Footer />);
