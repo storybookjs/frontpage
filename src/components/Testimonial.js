90c0986d14ad9d1e817a6c7936517179c21e4011
domyen <dom@hichroma.com>
2018-11-30 14:58:00 -0500

    Add Testimonial!

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,13 +1,123 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {
+  color,
+  spacing,
+  typography,
+  pageMargin,
+  pageMargins,
+  breakpoint,
+} from './../shared/styles';
 
-const Background = styled.div`
-  background: rebeccapurple;
-  margin-bottom: 1.45rem;
+import Avatar from './Avatar';
+
+const Quote = styled.div`
+  font-size: ${typography.size.m1}px;
+  line-height: ${typography.size.m3}px;
+  margin-bottom: 1.5rem;
+  max-width: 480px;
+  text-align: center;
 `;
 
-const Testimonial = ({ siteTitle }) => <Background>Testimonial</Background>;
+const Name = styled.div`
+  font-size: ${typography.size.s2}px;
+  font-weight: ${typography.weight.extrabold};
+`;
 
-export default Testimonial;
+const JobTitle = styled.div`
+  font-size: ${typography.size.s1}px;
+  color: ${color.mediumdark};
+`;
+
+const Meta = styled.div`
+  margin-left: 10px;
+`;
+const Author = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  border-left: 1px solid rgba(0, 0, 0, 0.05);
+  margin-left: 20px;
+  padding-left: 20px;
+  img {
+    display: inline-block;
+    width: auto;
+    max-height: 40px;
+  }
+`;
+const Attribution = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const HrWrapper = styled.div`
+  ${pageMargins};
+`;
+
+const Hr = styled.hr`
+  margin: 0;
+  display: block;
+`;
+
+const Inner = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  padding: 3rem ${spacing.padding.medium}px;
+  @media (min-width: ${breakpoint * 1}px) {
+    margin: 0 ${pageMargin * 3}%;
+    padding-bottom: 5rem;
+  }
+  @media (min-width: ${breakpoint * 2}px) {
+    margin: 0 ${pageMargin * 4}%;
+  }
+`;
+
+const Wrapper = styled.div``;
+
+export default function Testimonial({ text, avatarUrl, name, jobTitle, logo, ...props }) {
+  return (
+    <Wrapper {...props}>
+      <HrWrapper>
+        <Hr />
+      </HrWrapper>
+      <Inner>
+        <Quote>{text}</Quote>
+        <Attribution>
+          <Author>
+            <Avatar size="large" username={name} src={avatarUrl} />
+            <Meta>
+              <Name>{name}</Name>
+              <JobTitle>{jobTitle}</JobTitle>
+            </Meta>
+          </Author>
+          <Logo>
+            <img src={logo} alt="company logo" />
+          </Logo>
+        </Attribution>
+      </Inner>
+    </Wrapper>
+  );
+}
+
+Testimonial.propTypes = {
+  text: PropTypes.node,
+  avatarUrl: PropTypes.string,
+  name: PropTypes.string,
+  jobTitle: PropTypes.string,
+  logo: PropTypes.string,
+};
+
+Testimonial.defaultProps = {
+  text: null,
+  avatarUrl: null,
+  name: null,
+  jobTitle: null,
+  logo: null,
+};
---
+++ src/components/Testimonial.stories.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Testimonial from './Testimonial';
+
+storiesOf('Layout/Testimonial', module)
+  .addDecorator(storyFn => <div style={{ padding: '60px 0' }}>{storyFn()}</div>)
+  .add('default', () => (
+    <Testimonial
+      text={
+        <span>
+          “Storybook is an absolutely fantastic app that helps to keep us organized and our
+          customers happy.”
+        </span>
+      }
+      avatarUrl="https://avatars1.githubusercontent.com/u/263385?s=88&v=4"
+      name="Dominic Nguyen"
+      jobTitle="Product designer"
+      logo="/logo/user/logo-chromatic.svg"
+    />
+  ));
