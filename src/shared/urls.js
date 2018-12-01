079bfb03c776597a4c2859b08a869230c97849a2
domyen <dom@hichroma.com>
2018-12-01 16:17:19 -0500

    Moved urls from gatsby-config to a separate file
    Seems more straightforward to import them via JS rather than using StaticQuery

---
+++ src/shared/urls.js
@@ -0,0 +1,16 @@
+export const url = {
+  gitHub: 'https://github.com/storybooks/storybook/',
+
+  // Navigation
+  docs: 'https://storybook.js.org/basics/introduction/',
+  addons: '/addons',
+  community: '/community',
+  useCases: '/use-cases',
+  support: '/support',
+
+  // Social
+  blog: 'https://medium.com/storybookjs',
+  twitter: 'https://twitter.com/storybookjs',
+  chat: 'https://discord.gg/UUt2PJb',
+  youtube: 'https://www.youtube.com/channel/UCr7Quur3eIyA_oe8FNYexfg',
+};
