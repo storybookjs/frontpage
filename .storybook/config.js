e3a5962a4849a21c94cf0aba2b4c8381237c38df
Tom Coleman <tom@thesnail.org>
2018-12-02 14:45:45 +1100

    Update our gatsby integrationTo match those recommended by https://www.gatsbyjs.org/docs/visual-testing-with-storybook/

--- .storybook/config.js
+++ .storybook/config.js
@@ -11,12 +11,21 @@ addDecorator(story => (
   </Fragment>
 ));
 
-// Needed to add these to get Gatsby's links working.
-// We may need to do more: https://www.gatsbyjs.org/docs/unit-testing/
-window.__PATH_PREFIX__ = '/';
-window.___loader = {
-  enqueue: () => null,
+// Gatsby's Link overrides:
+// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
 };
+
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__PATH_PREFIX__ = '';
+
+// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname);
+};
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
 
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,20 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /gatsby\/cache-dir.*\.js$/,
-        loader: require.resolve('babel-loader'),
-      },
-    ],
-  },
-}
+module.exports = (baseConfig, env, defaultConfig) => {
+  // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+  defaultConfig.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+
+  // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+  defaultConfig.module.rules[0].use[0].loader = require.resolve('babel-loader');
+
+  // use @babel/preset-react for JSX and env (instead of staged presets)
+  defaultConfig.module.rules[0].use[0].options.presets = [
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-env'),
+  ];
+
+  // use @babel/plugin-proposal-class-properties for class arrow functions
+  defaultConfig.module.rules[0].use[0].options.plugins = [
+    require.resolve('@babel/plugin-proposal-class-properties'),
+  ];
+
+  return defaultConfig;
+};
