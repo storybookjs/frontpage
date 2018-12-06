26512a6305026c5ccbf3d677b7bba06f7b12c215
domyen <dom@hichroma.com>
2018-12-06 16:00:47 -0500

    FeaturesLayout component to hold all the features

---
+++ src/components/FeaturesLayout.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+import { spacing, pageMargin, breakpoint } from './../shared/styles';
+
+const Layout = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+
+  > * {
+    padding: 0 ${spacing.padding.medium}px 3rem;
+  }
+
+  @media (min-width: ${breakpoint * 1}px) {
+    padding-bottom: 3rem;
+  }
+
+  ${props =>
+    props.columns === 2 &&
+    css`
+      @media (min-width: ${breakpoint * 1}px) {
+        margin: 0 ${pageMargin * 3}%;
+        > * {
+          flex: 0 1 50%;
+        }
+      }
+      @media (min-width: ${breakpoint * 2}px) {
+        margin: 0 ${pageMargin * 4}%;
+      }
+    `};
+
+  ${props =>
+    props.columns === 3 &&
+    css`
+      @media (min-width: ${breakpoint * 1}px) {
+        margin: 0 ${pageMargin * 1}%;
+        > * {
+          flex: 0 1 33.33%;
+        }
+      }
+      @media (min-width: ${breakpoint * 2}px) {
+        margin: 0 ${pageMargin * 3}%;
+      }
+      @media (min-width: ${breakpoint * 4}px) {
+        margin: 0 ${pageMargin * 4}%;
+      }
+    `};
+`;
+
+export default function FeaturesLayout({ columns, children }) {
+  return <Layout columns={columns}>{children}</Layout>;
+}
+
+FeaturesLayout.propTypes = {
+  columns: PropTypes.number,
+  children: PropTypes.node,
+};
+
+FeaturesLayout.defaultProps = {
+  columns: 2,
+  children: null,
+};
---
+++ src/components/FeaturesLayout.stories.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Feature from './Feature';
+import FeaturesLayout from './FeaturesLayout';
+
+storiesOf('Screens/UseCasesScreen/FeaturesLayout', module)
+  .add('2 column', () => (
+    <FeaturesLayout columns={2}>
+      <Feature
+        image={<img src="images/colored-icons/direction.svg" />}
+        title="Develop for every use case"
+        desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+      />
+      <Feature
+        image={<img src="images/colored-icons/direction.svg" />}
+        title="Develop for every use case"
+        desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+      />
+    </FeaturesLayout>
+  ))
+  .add('3 column', () => (
+    <FeaturesLayout columns={3}>
+      <Feature
+        image={<img src="images/colored-icons/direction.svg" />}
+        title="Develop for every use case"
+        desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+      />
+      <Feature
+        image={<img src="images/colored-icons/direction.svg" />}
+        title="Develop for every use case"
+        desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+      />
+      <Feature
+        image={<img src="images/colored-icons/direction.svg" />}
+        title="Develop for every use case"
+        desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+      />
+    </FeaturesLayout>
+  ));
