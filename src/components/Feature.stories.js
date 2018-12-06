73b0ce18b73d0665cbccc7a5527feec4cffa21f3
domyen <dom@hichroma.com>
2018-12-06 16:00:09 -0500

    Add Features component

---
+++ src/components/Feature.stories.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Feature from './Feature';
+
+storiesOf('Screens/UseCasesScreen/Feature', module)
+  .add('default', () => (
+    <Feature
+      image={<img src="images/colored-icons/direction.svg" />}
+      title="Develop for every use case"
+      desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+    />
+  ))
+  .add('w/children', () => (
+    <Feature
+      image={<img src="images/colored-icons/direction.svg" />}
+      title="Develop for every use case"
+      desc="Storybook makes it dead simple to mock hard-to-reach states and edge cases"
+    >
+      <div>children go here</div>
+    </Feature>
+  ));
