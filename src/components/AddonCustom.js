f812a498dd7b3ab3ddb1e366aa89be1a3df4802a
domyen <dom@hichroma.com>
2018-12-08 14:11:18 -0500

    AddonCustom

---
+++ src/components/AddonCustom.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  background,
+  color,
+  spacing,
+  typography,
+  pageMargin,
+  pageMargins,
+  breakpoint,
+} from './../shared/styles';
+
+import Link from './Link';
+import Icon from './Icon';
+
+const Title = styled.div`
+  font-weight: ${typography.weight.black};
+  font-size: ${typography.size.m1}px;
+  line-height: 1;
+  margin-bottom: 0.25rem;
+`;
+
+const Desc = styled.div`
+  font-size: ${typography.size.s3}px;
+  line-height: 1.5;
+  margin-bottom: 0.25rem;
+`;
+
+const Meta = styled.div``;
+const Image = styled.img`
+  width: 192px;
+  margin-right: 30px;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  text-align: center;
+
+  padding: 3rem ${spacing.padding.medium}px;
+
+  @media (min-width: ${breakpoint * 1}px) {
+    margin: 0 ${pageMargin * 3}%;
+    text-align: left;
+  }
+
+  @media (min-width: ${breakpoint * 2}px) {
+    margin: 0 ${pageMargin * 4}%;
+  }
+
+  ${Meta} {
+    flex: 1;
+  }
+`;
+
+const Wrapper = styled.div`
+  background: ${background.app};
+  border-top: 1px solid ${color.border};
+  border-bottom: 1px solid ${color.border};
+`;
+
+export default function AddonCustom({ ...props }) {
+  return (
+    <Wrapper {...props}>
+      <Inner>
+        <Image src="images/addons/custom.svg" />
+        <Meta>
+          <Title>Create your own addon</Title>
+          <Desc>
+            Customize Storybook's UI, API, and create custom workflows by building your own addon
+          </Desc>
+          <Link>
+            Learn how to build an addon <Icon icon="arrowright" />
+          </Link>
+        </Meta>
+      </Inner>
+    </Wrapper>
+  );
+}
---
+++ src/components/AddonCustom.stories.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import AddonCustom from './AddonCustom';
+
+storiesOf('Screens/AddonScreen/AddonCustom', module).add('default', () => <AddonCustom />);
