df5a98e158eebd361d84d5307482e0417b7867e4
domyen <dom@hichroma.com>
2018-12-11 15:16:41 -0500

    Responsive AddonCustom

--- src/components/screens/AddonScreen/AddonCustom.js
+++ src/components/screens/AddonScreen/AddonCustom.js
@@ -7,21 +7,34 @@ const { background, color, spacing, typography, pageMargin, pageMargins, breakpo
 
 const Title = styled.div`
   font-weight: ${typography.weight.black};
-  font-size: ${typography.size.m1}px;
-  line-height: 1;
-  margin-bottom: 0.25rem;
+  font-size: ${typography.size.s3}px;
+  line-height: 1.5rem;
+
+  @media (min-width: ${breakpoint * 1}px) {
+    font-size: ${typography.size.m1}px;
+    line-height: ${typography.size.m2}px;
+    margin-bottom: 0.5rem;
+  }
 `;
 
 const Desc = styled.div`
   font-size: ${typography.size.s3}px;
   line-height: 1.5;
-  margin-bottom: 0.25rem;
+  @media (min-width: ${breakpoint * 1}px) {
+    margin-bottom: 0.5rem;
+  }
 `;
 
 const Meta = styled.div``;
 const Image = styled.img`
-  width: 192px;
-  margin-right: 30px;
+  display: block;
+  width: 100px;
+  margin: 0 20px;
+
+  @media (min-width: ${breakpoint * 1}px) {
+    width: 192px;
+    margin-right: 30px;
+  }
 `;
 
 const Inner = styled.div`
@@ -29,13 +42,12 @@ const Inner = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   align-items: center;
-  text-align: center;
+  text-align: left;
 
   padding: 3rem ${spacing.padding.medium}px;
 
   @media (min-width: ${breakpoint * 1}px) {
     margin: 0 ${pageMargin * 3}%;
-    text-align: left;
   }
 
   @media (min-width: ${breakpoint * 2}px) {
