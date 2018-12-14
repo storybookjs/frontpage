0a8ae150670ebb40db337251c0575b1c3b8387b8
Tom Coleman <tom@percolatestudio.com>
2018-12-14 14:07:46 +1100

    Remove page stories

--- src/pages/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import IndexPage from './index';
-
-//TODO allow Gatsby pages to render in SB
-
-storiesOf('Index', module).add('default', () => <IndexPage />);
