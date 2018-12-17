b240ff2a1cb90f05ed5829dfe5a11ef73c2460ab
Tom Coleman <tom@percolatestudio.com>
2018-12-17 14:37:24 +1100

    Add styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
