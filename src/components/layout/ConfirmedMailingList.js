39ed5ae58ad8bc22b62f473f7d0b7665205ef2e6
Tom Coleman <tom@thesnail.org>
2018-12-20 09:44:18 +1100

    Fix up mailinglist

--- src/components/layout/ConfirmedMailingList.js
+++ src/components/layout/ConfirmedMailingList.js
@@ -51,16 +51,16 @@ PureConfirmedMailingList.propTypes = {
 export default compose(
   withState('hasSubscribed', 'onSetHasSubscribed', false),
   withHandlers({
-    // XXX: where does this go?
-    // u=06a6fce3ab1327784d4342396
-    // id=18b5cea6e6
+    // eslint-disable-next-line no-unused-vars
     onSubscribe: ({ onSetHasSubscribed }) => async ({ email, optIn }) => {
       const data = new FormData();
       const fullFields = {
+        u: '06a6fce3ab1327784d4342396',
+        id: '18b5cea6e6',
         MERGE0: email,
         // XXX: this is for the opt in checkbox on things like free resources
-        // We don't have this right now, but may have it in the future
-        ...(optIn && { '': null }),
+        // We don't have this right now, but may have it in the future (need to change the group field)
+        // ...(optIn && { 'group[4969][1]': null }),
       };
       // e.g. u, id, SOURCE
       Object.keys(fullFields).forEach(key => data.append(key, fullFields[key]));
