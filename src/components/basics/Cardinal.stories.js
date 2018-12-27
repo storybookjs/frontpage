19e2267a53954968fc7dd93cb6c804739d7956f2
domyen <dom@hichroma.com>
2018-12-27 10:55:59 -0500

    Add `loading` story to Cardinal #17

--- src/components/basics/Cardinal.stories.js
+++ src/components/basics/Cardinal.stories.js
@@ -7,6 +7,7 @@ import Cardinal from './Cardinal';
 storiesOf('basics/Cardinal', module)
   .add('all cardinals', () => (
     <div style={{ background: 'white' }}>
+      <Cardinal loading size="small" />
       <Cardinal size="small" count="77" text="Default" noPlural status="default" />
       <Cardinal size="small" count="v5.0" text="Latest version" noPlural status="primary" />
       <Cardinal size="small" count="700k" text="Installs per month" noPlural status="secondary" />
@@ -18,6 +19,7 @@ storiesOf('basics/Cardinal', module)
       <Cardinal size="large" count="+500" text="Contributors" noPlural status="tertiary" />
     </div>
   ))
+  .add('loading', () => <Cardinal loading size="small" />)
   .add('w/link', () => (
     <Cardinal
       size="small"
