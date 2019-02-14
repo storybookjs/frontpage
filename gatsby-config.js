625b48ef6d5fee059aa92f53788891bd791d321b
domyen <dom@hichroma.com>
2019-02-14 15:18:26 -0500

    Add Google Analytics via gatsby package

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,10 +30,33 @@ module.exports = {
     {
       resolve: 'gatsby-source-github-repo',
       options: {
-        repoUrl: 'https://github.com/storybooks/storybook'
+        repoUrl: 'https://github.com/storybooks/storybook',
       },
     },
-    
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-134532782-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Enables Google Optimize using your container Id
+        optimizeId: null,
+        // Enables Google Optimize Experiment ID
+        experimentId: null,
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: null,
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'example.com',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
