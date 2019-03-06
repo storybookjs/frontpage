74dc615604e6d9c654c90efd89a93a5de59067b6
Michael Shilman <michael@lab80.co>
2019-03-06 17:11:33 +0800

    Pass analytics through segment for convenience

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,19 +34,9 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-segment',
       options: {
-        trackingId: 'UA-134532782-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Enables Google Optimize using your container Id
-        optimizeId: null,
-        // Enables Google Optimize Experiment ID
-        experimentId: null,
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: null,
+        writeKey: 'AvvBObOmHaEMqfub8JJUXq5umjsuaqS8',
       },
     },
     {
