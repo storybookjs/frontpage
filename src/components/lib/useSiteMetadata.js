c51ce16551da7166fe5a4f0c9d35c5eb0619d58f
Dale Blackburn <dale@dakebl.co.uk>
2019-04-18 18:16:22 +0100

    created useSiteMetadata hook

---
+++ src/components/lib/useSiteMetadata.js
@@ -0,0 +1,98 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useSiteMetadata = () => {
+  const result = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          ogImage
+          googleSiteVerification
+          latestVersion
+          urls {
+            gitHubOrg
+            homepageUrl
+            docsUrl
+            npmApiBase
+            navLinks {
+              title
+              href
+              isGatsby
+            }
+            gitHub {
+              repo
+              frontpage
+              issues
+              releases
+              contributors
+              brand
+            }
+            npm
+            openCollective
+            npmApi {
+              react
+              reactNative
+              vue
+              angular
+              ember
+              html
+              svelte
+              mithril
+              riot
+              polymer
+              preact
+            }
+            home
+            docs {
+              home
+              addonInstruction
+            }
+            addons
+            community
+            useCases
+            support
+            team
+            blog
+            medium
+            twitter
+            chat
+            youtube
+            brand
+            designSystem
+            badge
+            presentation
+            video
+            framework {
+              react
+              reactNative
+              vue
+              angular
+              ember
+              html
+              svelte
+              mithril
+              riot
+            }
+            officialAddons {
+              knobs
+              actions
+              source
+              info
+              viewport
+              storyshots
+              backgrounds
+              accessibility
+              console
+              links
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return result.site.siteMetadata;
+};
+
+export default useSiteMetadata;
