af4ab74647815123ed22c5c638c72229f734ed4b
Kyle Suss <susskyle@gmail.com>
2020-07-06 16:14:43 -0600

    Setup Gatsby infra, add old changelogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,75 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-exports.createPages = ({ actions }) => {
-  const { createRedirect } = actions;
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.onCreateNode = ({ actions, getNode, node }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: 'content',
+    });
+
+    const [pageType, version] = slug.split('/').filter(p => !!p);
+
+    createNodeField({ node, name: 'slug', value: slug });
+    createNodeField({ node, name: 'pageType', value: pageType });
+    createNodeField({ node, name: 'version', value: version });
+  }
+};
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createRedirect, createPage } = actions;
   createRedirect({
     fromPath: `/docs/`,
     isPermanent: true,
     redirectInBrowser: true,
     toPath: `/`,
+  });
+
+  return new Promise(resolve => {
+    graphql(`
+      {
+        pages: allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+                pageType
+                version
+              }
+            }
+          }
+        }
+      }
+    `).then(({ data: { pages: { edges } } }) => {
+      edges.forEach(({ node }, index) => {
+        const { pageType, slug, version } = node.fields;
+
+        createPage({
+          path: slug,
+          component: path.resolve(`./src/components/screens/ReleasesScreen/ReleasesScreen.js`),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            pageType,
+            slug,
+            version,
+          },
+        });
+
+        // Leave a /releases/ endpoint, but redirect it to the latest version
+        if (index + 1 === edges.length) {
+          createRedirect({
+            fromPath: `/releases/`,
+            isPermanent: false,
+            redirectInBrowser: true,
+            toPath: `/releases/${version}/`,
+          });
+        }
+      });
+
+      resolve();
+    });
   });
 };
---
+++ src/components/screens/ReleasesScreen/ReleasesScreen.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { Highlight, global, styles } from '@storybook/design-system';
+import styled from 'styled-components';
+
+import useSiteMetadata from '../../lib/useSiteMetadata';
+
+import TableOfContents from './TableOfContents';
+import { SocialGraph } from '../../basics';
+import PageLayout from '../../layout/PageLayout';
+import { releaseForamtting } from '../../../styles/formatting';
+
+const { GlobalStyle } = global;
+
+const { breakpoint, color, pageMargins, typography } = styles;
+
+const Content = styled.div`
+  ${pageMargins}
+  padding-top: 66px;
+  padding-bottom: 66px;
+
+  @media (min-width: ${breakpoint * 1.5}px) {
+    display: flex;
+  }
+`;
+
+const Sidebar = styled.div`
+  flex: 0 1 80px;
+  padding-right: 80px;
+`;
+
+const TOCHeader = styled.div`
+  color: ${color.dark};
+  font-size: ${typography.size.s3}px;
+  font-weight: ${typography.weight.bold};
+  line-height: 20px;
+`;
+
+const Title = styled.div`
+  color: ${color.darkest};
+  font-size: ${typography.size.l1}px;
+  font-weight: ${typography.weight.black};
+  letter-spacing: -0.33px;
+  line-height: 40px;
+  margin-bottom: 9px;
+`;
+
+const Release = styled.div`
+  ${releaseForamtting}
+`;
+
+function PureReleasesScreen({ data, ...props }) {
+  const { ogImage, urls = {} } = useSiteMetadata();
+  const { home } = urls;
+  const {
+    allReleases: { edges },
+    currentPage: {
+      html,
+      frontmatter: { title },
+      fields: { slug: currentPageSlug },
+    },
+  } = data;
+  const tocEntries = edges.map(({ node }) => ({
+    slug: node.fields.slug,
+    title: node.fields.version,
+  }));
+
+  return (
+    <>
+      <GlobalStyle />
+      <PageLayout {...props}>
+        <SocialGraph
+          title="Storybook: UI component explorer for frontend developers"
+          desc="Storybook is an open source tool for developing UI components in isolation for React, Vue, and Angular. It makes building stunning UIs organized and efficient."
+          url={home}
+          image={ogImage}
+        />
+        <Content>
+          <Sidebar>
+            <TOCHeader>Versions</TOCHeader>
+            <TableOfContents currentPageSlug={currentPageSlug} entries={tocEntries} />
+          </Sidebar>
+
+          <Release>
+            <Title>{title}</Title>
+            <Highlight>{html}</Highlight>
+          </Release>
+        </Content>
+      </PageLayout>
+    </>
+  );
+}
+
+PureReleasesScreen.propTypes = {
+  data: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+export default PureReleasesScreen;
+
+export const query = graphql`
+  query ReleasesScreenQuery($slug: String!) {
+    allReleases: allMarkdownRemark {
+      edges {
+        node {
+          html
+          fields {
+            slug
+            version
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+    currentPage: markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+      fields {
+        version
+        slug
+      }
+    }
+  }
+`;
