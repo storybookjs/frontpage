43bca16112cc8613d0cae3ebee064978d244824d
Tom Coleman <tom@thesnail.org>
2020-07-07 19:58:09 +1000

    Fix issue with `__BASE_PATH__~

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,7 +13,7 @@ WebFont.load({
 });
 
 export const decorators = [
-  story => (
+  (story) => (
     <Fragment>
       <GlobalStyle />
       {story()}
@@ -30,9 +30,10 @@ global.___loader = {
 
 // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
 global.__PATH_PREFIX__ = '';
+global.__BASE_PATH__ = '';
 
 // This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
-window.___navigate = pathname => {
+window.___navigate = (pathname) => {
   action('NavigateTo:')(pathname);
 };
 
