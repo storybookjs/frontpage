d4cb6bb8ad08d5efdb0349db6abc95d6668e379d
Tom Coleman <tom@thesnail.org>
2020-07-08 17:32:12 +1000

    Add gatsby plugin redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,5 +55,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
   ],
 };
