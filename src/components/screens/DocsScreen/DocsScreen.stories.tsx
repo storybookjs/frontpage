4f15bd29947a75efe3094ae33b17f8c5761606e3
Kyle Suss <susskyle@gmail.com>
2020-08-04 09:25:10 -0600

    Remove storiesOf, add decorator for DocsScreen

--- src/components/screens/DocsScreen/DocsScreen.stories.tsx
+++ src/components/screens/DocsScreen/DocsScreen.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
 
 import DocsScreen from './DocsScreen';
 
@@ -16,9 +16,14 @@ const tocItem = { githubUrl: 'github.com' };
 
 const nextTocItem = { path: '/path', title: 'Title', description: 'This is a description.' };
 
+const Wrapper = styled.div`
+  padding-bottom: 10px;
+`;
+
 export default {
   title: 'Frontpage|screens/DocsScreen/DocsScreen',
   component: DocsScreen,
+  decorators: [(storyFn) => <Wrapper>{storyFn()}</Wrapper>],
 };
 
 export const Base = () => <DocsScreen data={data} pageContext={{ tocItem: {} }} />;
