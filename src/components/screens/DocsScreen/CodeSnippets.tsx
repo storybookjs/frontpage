1bf858f0f1593f9904b0e831e83f1f608f3352be
Kyle Suss <susskyle@gmail.com>
2020-08-06 09:09:52 -0600

    Upgrade design system for markdown support

--- src/components/screens/DocsScreen/CodeSnippets.tsx
+++ src/components/screens/DocsScreen/CodeSnippets.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { Badge, CodeSnippets as DesignSystemCodeSnippets, styles } from '@storybook/design-system';
 
