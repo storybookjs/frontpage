eda5a486384e941dae73e4a9df21059092555f9b
Tom Coleman <tom@thesnail.org>
2020-08-10 15:37:51 +1000

    Fix broken story

--- src/components/screens/DocsScreen/FrameworkSelector.stories.tsx
+++ src/components/screens/DocsScreen/FrameworkSelector.stories.tsx
@@ -24,7 +24,7 @@ export const Base = Template.bind({});
 Base.args = {
   coreFrameworks,
   communityFrameworks,
-  currentFramework: frameworks[0],
+  currentFramework: coreFrameworks[0],
   slug: '/slug',
   tooltipProps: { startOpen: true },
 };
