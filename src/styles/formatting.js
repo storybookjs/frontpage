546200294b5dfe9cf8db09a5d000c8bcf61f4767
domyen <dom@hichroma.com>
2020-08-10 14:37:05 -0400

    Remove unused import

--- src/styles/formatting.js
+++ src/styles/formatting.js
@@ -1,5 +1,5 @@
 import { css } from 'styled-components';
-import { styles, CodeSnippet } from '@storybook/design-system';
+import { styles } from '@storybook/design-system';
 import { darken } from 'polished';
 
 const { color, typography } = styles;
