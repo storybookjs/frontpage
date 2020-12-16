95712ff93870030df4b1c2a590326c40afac9841
winkerVSbecks <varunvachhar@gmail.com>
2020-12-16 15:10:00 -0500

    use absolute url's for og images

--- src/components/basics/SocialGraph.tsx
+++ src/components/basics/SocialGraph.tsx
@@ -1,19 +1,24 @@
 import React, { FunctionComponent } from 'react';
 import Helmet from 'react-helmet';
+import useSiteMetadata from '../lib/useSiteMetadata';
 
-const SocialGraph: FunctionComponent<Props> = ({ title, desc, url = null, image = null }) => (
+const SocialGraph: FunctionComponent<Props> = ({ title, desc, url = null, image = null }) => {
+  const { siteUrl } = useSiteMetadata();
+
+  return (
   <Helmet title={title}>
     <meta name="description" content={desc} />
     {url && <meta property="og:url" content={url} />}
-    {image && <meta property="og:image" content={image} />}
-    {image && <meta name="twitter:image" content={image} />}
+    {image && <meta property="og:image" content={`${siteUrl}/${image}`} />}
+    {image && <meta name="twitter:image" content={`${siteUrl}/${image}`} />}
 
     <meta property="og:title" content={title} />
     <meta property="og:description" content={desc} />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={desc} />
+    <meta name="twitter:site" content="@storybookjs" />
   </Helmet>
-);
+)};
 
 export default SocialGraph;
 
