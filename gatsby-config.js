ae3b794734c4befd6c53f163cdf6b1235dfeec42
winkerVSbecks <varunvachhar@gmail.com>
2021-03-01 13:07:02 -0500

    Avoid stripping .md for absolute github links

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,7 +90,7 @@ module.exports = {
               // Strip off trailing .md from links. We want them there in the original source so the
               // relative links work inside github, when you view the .md files directly
               // (Note we need to ensure we don't match the links below)
-              pattern: /^(?!\.\.\/\.\.\/)(.*)\.md(#.*)?$/,
+              pattern: /^(?!\.\.\/\.\.\/)(?!https:\/\/github\.com\/)(.*)\.md(#.*)?$/,
               replace: '$1$2',
             },
           },
