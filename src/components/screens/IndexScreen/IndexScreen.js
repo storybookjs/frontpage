baa74b0ce8194e0f03e39e732e1b9921cdc1376b
domyen <dom@hichroma.com>
2021-05-12 15:59:23 -0400

    IndexScreen: update copy to be clearer and reflect Storybooks current features

--- src/components/screens/IndexScreen/IndexScreen.js
+++ src/components/screens/IndexScreen/IndexScreen.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -6,6 +6,7 @@ import { Button, Link, styles } from '@storybook/design-system';
 import useSiteMetadata from '../../lib/useSiteMetadata';
 
 import { SocialGraph, Video } from '../../basics';
+import GatsbyLink from '../../basics/GatsbyLink';
 import Hero from './Hero';
 import SocialProof from '../../layout/SocialProof';
 import PlaceholderAspectRatio from '../../layout/PlaceholderAspectRatio';
@@ -44,7 +45,8 @@ export function PureIndexScreen({ ...props }) {
     <>
       <SocialGraph
         title="Storybook: UI component explorer for frontend developers"
-        desc="Storybook is an open source tool for developing UI components in isolation for React, Vue, and Angular. It makes building stunning UIs organized and efficient."
+        desc="Storybook is an open source tool for building UI components and pages in isolation. It
+          streamlines UI development, testing, and documentation."
         url={home}
         image={ogImage}
       />
@@ -52,7 +54,6 @@ export function PureIndexScreen({ ...props }) {
       <Hero />
 
       <SocialProof
-        heading="Trusted by"
         path="images/logos/user"
         brands={['github', 'dropbox', 'airbnb', 'lyft', 'mozilla', 'jetbrains']}
         monochrome
@@ -71,15 +72,15 @@ export function PureIndexScreen({ ...props }) {
             />
           </Placeholder>
         }
-        title="Deliver robust UIs"
-        desc="Storybook provides a sandbox to build UI components in isolation so you can develop hard-to-reach states and edge cases."
+        title="Develop durable UIs"
+        desc="Storybook provides a sandbox to build UIs in isolation so you can develop hard-to-reach states and edge cases."
         lazyloadPlaceholder={<Placeholder ratio={0.75} />}
       />
       <BenefitList>
         <BenefitItem
           image={<img src="/images/home/build-canvas.png" alt="isolate" />}
-          title="Build components in isolation"
-          desc="Create components without needing to stand up screens, fuss with data, or build business logic."
+          title="Build UIs in isolation"
+          desc="Implement components and pages without needing to fuss with data, APIs, or business logic."
         />
         <BenefitItem
           image={<img src="/images/home/build-cases.png" alt="mock states" />}
@@ -90,19 +91,19 @@ export function PureIndexScreen({ ...props }) {
           image={<img src="/images/home/build-sidebar.png" alt="save use cases" />}
           title="Document use cases as stories"
           desc={
-            <Fragment>
+            <>
               Save use cases as stories in{' '}
               <Link href="https://storybook.js.org/basics/writing-stories/" target="_blank">
                 plain JavaScript
               </Link>{' '}
               to revisit during development, testing, and QA.
-            </Fragment>
+            </>
           }
         />
         <BenefitItem
           image={<img src="/images/home/build-addons.png" alt="extend with addons" />}
           title="Supercharge your workflow with addons"
-          desc="Use addons to build UI faster, document a component library, and streamline your workflow."
+          desc="Use addons to customize your workflow, automate testing, and integrate with your favorite tools."
         />
       </BenefitList>
       <Testimonial
@@ -131,46 +132,50 @@ export function PureIndexScreen({ ...props }) {
               />
             </Placeholder>
           }
-          title="Ship components with confidence"
-          desc="Storybook makes it easy to keep track of edge cases and as a result you get tests for free."
+          title="Test UIs with less effort and no flake"
+          desc="Stories are a pragmatic, reproducible way to keep track of UI edge cases. Write stories once then reuse them to power automated tests."
           lazyloadPlaceholder={<Placeholder ratio={0.75} />}
         />
         <BenefitList>
           <BenefitItem
             image={<img src="/images/home/test-visual.png" alt="visual test" />}
-            title="Ensure consistent user experiences"
-            desc="Whenever you write a story you get a handy visual spec. Quickly browse stories to make sure your UI looks right."
-          />
-          <BenefitItem
-            image={<img src="/images/home/test-snapshot.png" alt="snapshot test" />}
-            title="Auto-detect code regressions"
-            desc="Get code snapshot tests out of the box with Storyshots, an official addon."
-          />
-          <BenefitItem
-            image={<img src="/images/home/test-unit.png" alt="unit test" />}
-            title="Unit test components"
-            desc="Reuse stories in your unit tests to confirm nuanced functionality."
+            title="Spot test in a glance"
+            desc="Whenever you write a story you get a handy test case. Quickly browse stories to make sure your UI looks right."
           />
           <BenefitItem
             image={
               <img src="/images/home/test-visual-regression.png" alt="visual regression test" />
             }
-            title="Catch UI changes down to the pixel every commit"
-            desc="Pinpoint UI changes with visual testing tools."
+            title="Visual test appearance"
+            desc="Pinpoint UI changes down to the pixel by comparing image snapshots of stories."
+          />
+          <BenefitItem
+            image={<img src="/images/home/test-unit.png" alt="unit test" />}
+            title="Unit test functionality"
+            desc="Reuse stories in your unit tests to confirm nuanced functionality."
+          />
+          <BenefitItem
+            image={<img src="/images/home/test-accessibility.png" alt="accessibility test" />}
+            title="Accessibility test"
+            desc={
+              <>
+                Check stories for WCAG and ARIA issues with the{' '}
+                <GatsbyLink to="/addons/@storybook/addon-a11y/">A11y addon</GatsbyLink>.
+              </>
+            }
           />
         </BenefitList>
 
         <Testimonial
           text={
             <span>
-              “Storybook has made developing components more streamlined by allowing us to easily
-              include technical documentation within our design system!”
+              “Storybook is crucial to our UX process. The “old way” seems ludicrous now!”
             </span>
           }
-          avatarUrl="https://avatars0.githubusercontent.com/u/3028593?s=460&v=4"
-          name="Taurie Davis"
-          jobTitle="Author of Building Design Systems"
-          logo={GitlabLogoSVG}
+          avatarUrl="https://avatars3.githubusercontent.com/u/31106469?s=100&v=4"
+          name="Andrew Frankel"
+          jobTitle="Principal engineer"
+          logo={SalesForceLogoSVG}
         />
 
         <Separator />
@@ -187,25 +192,21 @@ export function PureIndexScreen({ ...props }) {
             />
           </Placeholder>
         }
-        title="Share and reuse everything"
-        desc={
-          <div>
-            Stop reinventing the wheel. Get a birds-eye view of your UI library to find components
-            to reuse in your project.
-          </div>
-        }
+        title="Document UI to share with your team"
+        desc="Stories show how UI actually works not just how it's supposed to work. That makes
+            gathering feedback and reproductions easy."
         lazyloadPlaceholder={<Placeholder ratio={0.75} />}
       />
       <BenefitList>
         <BenefitItem
           image={<img src="/images/home/share-search.png" alt="search stories" />}
-          title="Find any component in your app"
-          desc="Storybook is a searchable, single source of truth for your UI components."
+          title="Find any component or page in your app"
+          desc="Storybook is a searchable, single source of truth for your UI."
         />
         <BenefitItem
           image={<img src="/images/home/share-collaborate.png" alt="collaborate" />}
           title="Get timely feedback during development"
-          desc="Collaborate on UI implementation with your team by deploying Storybook to the cloud."
+          desc="Publish Storybook online to give your team a universal reference point for feedback."
         />
         <BenefitItem
           image={<img src="/images/home/share-reuse.png" alt="share stories" />}
@@ -214,36 +215,35 @@ export function PureIndexScreen({ ...props }) {
         />
         <BenefitItem
           image={<img src="/images/home/share-document.png" alt="create a styleguide" />}
-          title="Generate a styleguide automatically"
+          title="Generate a UI docs automatically"
           desc={
             <>
-              Write Markdown/MDX to generate a customizable site that is perfect for component
-              libraries and design systems.{' '}
-              <Link
-                href="https://github.com/storybookjs/storybook/tree/next/addons/docs"
-                target="_blank"
-                withArrow
-              >
-                Learn more
-              </Link>
+              Write Markdown/MDX to generate a customizable site for component libraries and design
+              systems with our{' '}
+              <GatsbyLink to="/addons/@storybook/addon-docs/">Docs addon</GatsbyLink>.
             </>
           }
         />
       </BenefitList>
+
       <Testimonial
         text={
-          <span>“Storybook is crucial to our UX process. The “old way” seems ludicrous now!”</span>
+          <span>
+            “Storybook has made developing components more streamlined by allowing us to easily
+            include technical documentation within our design system!”
+          </span>
         }
-        avatarUrl="https://avatars3.githubusercontent.com/u/31106469?s=100&v=4"
-        name="Andrew Frankel"
-        jobTitle="Principal engineer"
-        logo={SalesForceLogoSVG}
+        avatarUrl="https://avatars0.githubusercontent.com/u/3028593?s=460&v=4"
+        name="Taurie Davis"
+        jobTitle="Author of Building Design Systems"
+        logo={GitlabLogoSVG}
       />
+
       <CTA
         text={<span>Storybook is quick to install and it’s easy to get started.</span>}
         action={
           <Button appearance="secondary" isLink href={docs}>
-            Get started
+            Go to the docs
           </Button>
         }
       />
