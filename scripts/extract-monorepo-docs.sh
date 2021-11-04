437f12757de24538030c07be4fe811875f192d5e
Kyle Gach <kyle.gach@gmail.com>
2021-11-03 21:39:19 -0600

    Extend `extract-monorepo-docs`
    - Still pulls from main by default
    - Will pull from relevant monorepo branch based on env.BRANCH
        - That env var is a Netlify convention

--- scripts/extract-monorepo-docs.sh
+++ scripts/extract-monorepo-docs.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -
 
-# Pull the monorepo docs from the branch specified as the first argument, or `master`.
+# Pull the monorepo docs from the branch specified as the first argument, or `main`.
 
 MONOREPO_CODELOAD_URL='https://codeload.github.com/storybookjs/storybook/tar.gz'
-BRANCH=${1:-main}
-TAR_NAME="storybook-$BRANCH"
-REPO_SUBDIR='docs/'
-REPO_DEPTH=2 # number of dirs + 1
+
+SB_REPO_SUBDIR='docs/'
+SB_REPO_DEPTH=2 # number of dirs + 1
 REPO_DIRNAME='src/content/docs'
+
+MAPPED_BRANCH=main
+if  [[ "$BRANCH" == "release-"* ]]; then
+  MAPPED_BRANCH=$BRANCH
+fi
+FINAL_BRANCH=${1:-$MAPPED_BRANCH}
+BRANCH_TAR_NAME="storybook-$FINAL_BRANCH"
 
 set -e
 
+echo "Extracting docs from $FINAL_BRANCH branch..."
+
 mkdir -p $REPO_DIRNAME
 rm -rf $REPO_DIRNAME/*
-curl $MONOREPO_CODELOAD_URL/$BRANCH | tar -zxvC $REPO_DIRNAME --strip $REPO_DEPTH $TAR_NAME/$REPO_SUBDIR
+curl $MONOREPO_CODELOAD_URL/$FINAL_BRANCH | tar -zxvC $REPO_DIRNAME --strip $SB_REPO_DEPTH $BRANCH_TAR_NAME/$SB_REPO_SUBDIR
