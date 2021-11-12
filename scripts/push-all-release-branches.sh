adbb61464e74a07da8d03bbe77d57921448bd8e6
Kyle Gach <kyle.gach@gmail.com>
2021-11-11 19:56:55 -0700

    Fix workflows
    - Ensure branches not on remote are pushed

--- scripts/push-all-release-branches.sh
+++ scripts/push-all-release-branches.sh
@@ -14,13 +14,15 @@ LATEST_VERSION=($(echo $LATEST_VERSION_FULL | sed -E 's/"version": "([0-9]+\.[0-
 VERSION_FILES=$(ls ./src/content/releases)
 EARLIEST_DOCS_VERSION=6.0
 
+set +e
+
 for VERSION_FILE in ${VERSION_FILES[@]}
 do
   VERSION=${VERSION_FILE/.md/''}
   if awk "BEGIN {exit !($VERSION >= $EARLIEST_DOCS_VERSION)}"; then
     if awk "BEGIN {exit !($VERSION != $LATEST_VERSION)}"; then
       VERSION_BRANCH=($(echo $VERSION | sed -E 's/([0-9]+)\.([0-9]+)/release-\1-\2/'))
-      REMOTE_BRANCH=$(git branch -a | grep remotes/origin/$VERSION_BRANCH)
+      REMOTE_BRANCH=$(git branch -r | grep origin/$VERSION_BRANCH)
       if [[ ! -z $REMOTE_BRANCH ]]; then git push origin --delete $VERSION_BRANCH; fi
       echo "Pushing branch $VERSION_BRANCH..."
       git push -f origin HEAD:$VERSION_BRANCH
