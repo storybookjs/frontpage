142a02c48c3bbb2b5c0d304eeefde04809cbdb37
Kyle Gach <kyle.gach@gmail.com>
2022-03-09 21:09:39 -0700

    Fetch latest blog post from Ghost
    - Update README

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
 import React, { Fragment } from 'react';
+import { graphql } from 'gatsby';
 import { global } from '@storybook/design-system';
 
 import IndexScreen from '../components/screens/IndexScreen/IndexScreen';
 
 const { GlobalStyle } = global;
 
-// In theory we could pass in props that we'd fetched via Gatsby's GraphQL
-export default () => (
-  <Fragment>
-    <GlobalStyle />
-    <IndexScreen />
-  </Fragment>
-);
+export default ({ data }) => {
+  const {
+    latestBlogPosts: { edges },
+  } = data;
+  const latestBlogPost = edges[0]?.node;
+
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <IndexScreen latestBlogPost={latestBlogPost} />
+    </Fragment>
+  );
+};
+
+export const query = graphql`
+  query IndexScreenQuery {
+    latestBlogPosts: allGhostPost(sort: { order: DESC, fields: [published_at] }, limit: 1) {
+      edges {
+        node {
+          slug
+          title
+        }
+      }
+    }
+  }
+`;
