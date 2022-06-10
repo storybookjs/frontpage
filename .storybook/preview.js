72aa3e87b75651a62d64dd3d1d03bf3652e4b53a
winkerVSbecks <varunvachhar@gmail.com>
2022-06-09 21:51:35 -0400

    upgrade and fix storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -33,19 +33,18 @@ export const decorators = [
 ];
 
 // Gatsby's Link overrides:
-// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+// Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
+// This global object isn't set in storybook context, requiring you to override it to empty functions (no-op),
+// so Gatsby Link doesn't throw errors.
 global.___loader = {
   enqueue: () => {},
   hovering: () => {},
 };
-
-// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+// This global variable prevents the "__BASE_PATH__ is not defined" error inside Storybook.
 global.__PATH_PREFIX__ = '';
-global.__BASE_PATH__ = '';
-
-global.__BASE_PATH__ = '';
-
-// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+global.__BASE_PATH__ = '/';
+// Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
+// In Storybook, it makes more sense to log an action than doing an actual navigate. Check out the actions addon docs for more info: https://storybook.js.org/docs/react/essentials/actions
 window.___navigate = (pathname) => {
   action('NavigateTo:')(pathname);
 };
