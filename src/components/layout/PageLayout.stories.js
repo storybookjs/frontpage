3db5efaae613237d7ea24c014994642de168a6be
winkerVSbecks <varunvachhar@gmail.com>
2022-08-31 10:32:30 -0400

    fix page layout stories

--- src/components/layout/PageLayout.stories.js
+++ src/components/layout/PageLayout.stories.js
@@ -9,12 +9,26 @@ export default {
   component: PageLayout,
 };
 
-export const Base = () => <PageLayout pageContext={{}}>children</PageLayout>;
+export const Base = () => (
+  <PageLayout pageContext={{}} location={{ pathname: '/' }}>
+    <div style={{ height: 600, border: '1px solid #ccc', color: '#fff' }}>children</div>
+  </PageLayout>
+);
+Base.parameters = {
+  backgrounds: { default: 'dark' },
+};
 
 export const DocsLayout = () => (
-  <PageLayout pageContext={{ ...docsPageContext, layout: 'docs' }}>children</PageLayout>
+  <PageLayout
+    pageContext={{ ...docsPageContext, layout: 'docs' }}
+    location={{ pathname: '/docs/' }}
+  >
+    <div style={{ height: 600, border: '1px solid #ccc' }}>children</div>
+  </PageLayout>
 );
 
 export const IframeLayout = () => (
-  <PageLayout pageContext={{ layout: 'iframe' }}>children</PageLayout>
+  <PageLayout pageContext={{ layout: 'iframe' }} location={{ pathname: '/releases/' }}>
+    <div style={{ height: 600, border: '1px solid #ccc' }}>children</div>
+  </PageLayout>
 );
