f88169f285d78312ad394bcf3cf6206fd3357e87
winkerVSbecks <varunvachhar@gmail.com>
2022-09-06 22:26:37 -0400

    404 page puzzle pieces animation

--- src/components/screens/NotFoundScreen/NotFoundScreen.js
+++ src/components/screens/NotFoundScreen/NotFoundScreen.js
@@ -2,21 +2,52 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { styled } from '@storybook/theming';
 
-import { Link, styles } from '@storybook/design-system';
-import PageTitle from '../../layout/PageTitle';
+import { Link } from '@storybook/design-system';
+import { styles } from '@storybook/components-marketing';
 import Feature from '../../layout/Feature';
 import FeaturesLayout from '../../layout/FeaturesLayout';
 
 import useSiteMetadata from '../../lib/useSiteMetadata';
 import GitHubSVG from '../../../images/logos/social/github.svg';
 import DiscordSVG from '../../../images/logos/social/discord.svg';
+import { PuzzlePieces } from './PuzzlePieces';
 
-const { breakpoint } = styles;
+const { breakpoint, marketing, text, color } = styles;
 
 const Features = styled(FeaturesLayout)`
   @media (min-width: ${breakpoint * 1}px) {
     margin-bottom: 5rem;
   }
+`;
+
+const Hero = styled.div`
+  max-width: 460px;
+  margin: 5rem auto;
+  position: relative;
+`;
+const Copy = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  pointer-events: none;
+`;
+
+const Title = styled.h1`
+  ${marketing.hero1};
+  color: ${color.midnight};
+`;
+const Description = styled.p`
+  ${text.large};
+  margin: 0;
+  text-align: center;
+  color: ${color.darker};
 `;
 
 export function PureNotFoundScreen({ ...props }) {
@@ -28,12 +59,17 @@ export function PureNotFoundScreen({ ...props }) {
         <meta name="robots" content="noindex" />
       </Helmet>
 
-      <PageTitle
-        heading="404"
-        title="Yikes, this is embarassing"
-        desc="Try double-checking the link or going back."
-        color="purple"
-      />
+      <Hero>
+        <PuzzlePieces />
+        <Copy>
+          <Title>404</Title>
+          <Description>
+            The page you were looking for couldn’t be found. It may have moved. Try double-checking
+            the link or going back.
+          </Description>
+        </Copy>
+      </Hero>
+
       <Features columns={2}>
         <Feature
           image={<img src={GitHubSVG} alt="GitHub" />}
