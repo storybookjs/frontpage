adbe7639f2c5f8df047734984bd8b5fbea807abe
Shaun Lloyd <shaun@chromatic.com>
2022-11-17 10:22:35 -0500

    Add authors to recipeItem stories

--- src/components/layout/recipes/RecipeItem.stories.js
+++ src/components/layout/recipes/RecipeItem.stories.js
@@ -11,6 +11,29 @@ export default {
   },
 };
 
+const authors = [
+  {
+    id: '1',
+    name: 'Dominic Nguyen',
+    avatarUrl: 'https://avatars2.githubusercontent.com/u/263385',
+  },
+  {
+    id: '2',
+    name: 'Tom Coleman',
+    avatarUrl: 'https://avatars2.githubusercontent.com/u/132554',
+  },
+  {
+    id: '3',
+    name: 'Zoltan Olah',
+    avatarUrl: 'https://avatars0.githubusercontent.com/u/81672',
+  },
+  {
+    id: '4',
+    name: 'Tim Hingston',
+    avatarUrl: 'https://avatars3.githubusercontent.com/u/1831709',
+  },
+];
+
 const Wrapper = styled.div`
   padding: 16px;
 
@@ -41,6 +64,7 @@ WithImage.args = {
   displayName: 'Next.js',
   description: 'A low config way to get your Next.js UI working in Storybook',
   views: 17143,
+  authors,
 };
 
 export const WithoutImage = Template.bind({});
@@ -48,6 +72,7 @@ WithoutImage.args = {
   displayName: 'Material UI',
   description: 'Get the most out of Material UI in your Storybook',
   views: 238,
+  authors,
 };
 
 export const Loading = Template.bind({});
@@ -63,12 +88,12 @@ const StatsWrapper = styled.div`
 
 export const StatVariations = (args) => (
   <StatsWrapper>
-    <RecipeItem {...args} views={104} />
-    <RecipeItem {...args} views={726} />
-    <RecipeItem {...args} views={5026} />
-    <RecipeItem {...args} views={17143} />
-    <RecipeItem {...args} views={171043} />
-    <RecipeItem {...args} views={3871043} />
+    <RecipeItem {...args} views={104} authors={authors.slice(0, 2)} />
+    <RecipeItem {...args} views={726} authors={authors.slice(0, 1)} />
+    <RecipeItem {...args} views={5026} authors={authors} />
+    <RecipeItem {...args} views={17143} authors={authors.slice(1, 2)} />
+    <RecipeItem {...args} views={171043} authors={authors} />
+    <RecipeItem {...args} views={3871043} authors={authors.slice(1, 2)} />
   </StatsWrapper>
 );
 StatVariations.args = {
