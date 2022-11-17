17b3f8a471f547570fdd96a3b504e7db9752de82
Shaun Lloyd <shaun@chromatic.com>
2022-11-17 11:54:48 -0500

    Update breadcrumb copy

--- src/util/generate-breadcrumb.js
+++ src/util/generate-breadcrumb.js
@@ -2,10 +2,10 @@ export function generateBreadcrumb(locationState) {
   return locationState?.from?.link
     ? {
         link: locationState.from.link,
-        title: `Back to ${locationState.from.title}`,
+        title: `Back to ${locationState.from.title} integrations`,
       }
     : {
         link: '/integrations/',
-        title: 'View full catalog',
+        title: 'Back to integrations',
       };
 }
