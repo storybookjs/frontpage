238f6d6be41272ae862f7ce8ab31cfc9eb4222a3
Kyle Gach <kyle.gach@gmail.com>
2022-12-12 11:03:38 -0700

    Upgrade to SB 7 beta

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-param-reassign */
 const webpack = require('webpack');
-
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../static'],
   addons: ['@storybook/addon-essentials'],
-  core: {
-    builder: 'webpack5',
-  },
+  core: {},
   webpack: async (config) => {
     config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
 
@@ -28,17 +25,18 @@ module.exports = {
         require.resolve('./useSiteMetadata')
       )
     );
-
     config.resolve.alias['../../../hooks/use-addons-search'] = require.resolve(
       './use-addons-search.mock.js'
     );
-
     config.plugins.unshift(
       new webpack.DefinePlugin({
         'process.env.GATSBY_ALGOLIA_API_KEY': JSON.stringify(process.env.GATSBY_ALGOLIA_API_KEY),
       })
     );
-
     return config;
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
 };
