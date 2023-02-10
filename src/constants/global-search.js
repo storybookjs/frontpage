e47dd45b3391e62a3c6e06296e54d22549cedbb0
Shaun Lloyd <shaun@chromatic.com>
2023-02-10 08:55:17 -0500

    Update docusearch meta

---
+++ src/constants/global-search.js
@@ -0,0 +1,10 @@
+export const GLOBAL_SEARCH_META_KEYS = {
+  FRAMEWORK: 'docsearch:framework',
+  VERSION: 'docsearch:version',
+  IMPORTANCE: 'docsearch:importance',
+};
+
+export const GLOBAL_SEARCH_IMPORTANCE = {
+  DOCS: 0,
+  RECIPE: 1,
+};
