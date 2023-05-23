7f066e68fce33d25f8bd388b0d2480e9a2cfe9bd
Kyle Gach <kyle.gach@gmail.com>
2023-05-23 11:04:37 -0600

    Fix snippetType in tracking
    - Remove negative lookahead regex, which isn't supported in older Safari

--- src/util/custom-events.ts
+++ src/util/custom-events.ts
@@ -1,17 +1,5 @@
-/**
- * In: get-started/installation-command-section/angular.mdx
- * Out: installation-command-section
- *
- * In: common/init-command.npx.js.mdx
- * Out: init-command
- *
- * In: malformed-snippet.mdx
- * Out: null
- */
-const SNIPPET_TYPE_REGEX = /(?<=\/)[^/.]+/;
-
 export function logSnippetInteraction(framework, snippetPath) {
-  const snippetType = snippetPath.match(SNIPPET_TYPE_REGEX)?.[0];
+  const [, snippetType] = snippetPath.split('/');
 
   if (typeof window !== 'undefined' && (window as any).gtag && snippetType) {
     (window as any).gtag('event', 'click_snippet', { framework, snippet_type: snippetType });
