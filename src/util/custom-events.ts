13e3d51f14d4e6c60aa76a869001b4a89e5ec34c
winkerVSbecks <varunvachhar@gmail.com>
2023-05-23 13:57:30 -0400

    strip extension from parsed snippetType and adds tests for snippetType parsing

--- src/util/custom-events.ts
+++ src/util/custom-events.ts
@@ -1,5 +1,25 @@
-export function logSnippetInteraction(framework, snippetPath) {
+export function extractSnippetPath(snippetPath: string) {
   const [, snippetType] = snippetPath.split('/');
+  if (!snippetType) {
+    return null;
+  }
+  const snippetTypeWithoutExtension = snippetType.replace(/\..+$/, '');
+
+  return snippetTypeWithoutExtension;
+}
+
+/**
+ * In: get-started/installation-command-section/angular.mdx
+ * Out: installation-command-section
+ *
+ * In: common/init-command.npx.js.mdx
+ * Out: init-command
+ *
+ * In: malformed-snippet.mdx
+ * Out: null
+ */
+export function logSnippetInteraction(framework, snippetPath) {
+  const snippetType = extractSnippetPath(snippetPath);
 
   if (typeof window !== 'undefined' && (window as any).gtag && snippetType) {
     (window as any).gtag('event', 'click_snippet', { framework, snippet_type: snippetType });
