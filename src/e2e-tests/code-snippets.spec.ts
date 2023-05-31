59bdf17a311c8b41e98f1b3c793ce67930acc6ee
Kyle Gach <kyle.gach@gmail.com>
2023-05-31 14:34:28 -0600

    Add test-archiver

--- src/e2e-tests/code-snippets.spec.ts
+++ src/e2e-tests/code-snippets.spec.ts
@@ -1,9 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, takeArchive } from '@chromaui/test-archiver';
 
-test('Snippets on writing-stories/introduction', async ({ page }) => {
+test('Snippets on writing-stories/introduction', async ({ page }, testInfo) => {
   await page.goto('/docs/react/writing-stories/introduction');
 
   await expect(page).toHaveTitle(/How to write stories/);
+  await takeArchive(page, 'With TS snippets', testInfo);
 
   const codeLanguageSelector = await page.getByRole('button', { name: 'TypeScript' });
   await codeLanguageSelector.click();
