1cf77a87a983cc1de235f743a31eda9debe667a7
Shaun Lloyd <shaun@chromatic.com>
2023-11-09 16:32:28 -0500

    Scaffold out tests for parse-snippet-content

---
+++ src/components/basics/CodeSnippets/utils/parse-snippet-content.utilts.test.ts
@@ -0,0 +1,6 @@
+import { trimSnippet, parseSnippetContent } from './parse-snippet-content.utils';
+
+describe('UTILITIES: parse-snippet-content.utils', () => {
+  describe('UTIL: trimSnippet', () => {});
+  describe('UTIL: parseSnippetContent', () => {});
+});
