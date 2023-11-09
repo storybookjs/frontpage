d0b4b1a78e68779e5e963e6d49c86d2e400d7266
Shaun Lloyd <shaun@chromatic.com>
2023-11-09 16:33:44 -0500

    Scaffold complex codeSnippet component

---
+++ src/components/basics/CodeSnippets/CodeSnippet.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface CodeSnippetProps {
+  paths: string[];
+  isTerminalSnippet?: boolean;
+}
+
+// TODO: Implement complex MDX codeSnippets with multiple paths
+export const CodeSnippet = ({ paths, isTerminalSnippet }: CodeSnippetProps) => {
+  return null;
+};
