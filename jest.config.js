451cabb08bbceb2e683e6bc7b6f3a94090556004
Shaun Lloyd <shaun@chromatic.com>
2023-11-17 10:32:54 -0500

    Fix broken test suites from gatsby packages

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,14 @@ module.exports = {
   snapshotFormat: {
     printBasicPrototype: false,
   },
+  moduleNameMapper: {
+    '^gatsby-core-utils/(.*)$': `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    '^gatsby-plugin-utils/(.*)$': [`gatsby-plugin-utils/dist/$1`, `gatsby-plugin-utils/$1`],
+    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`,
+  },
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
 };
