7ab08b63fd61e65e2e0f54b1c961bdb9732547fe
Kyle Gach <kyle.gach@gmail.com>
2023-11-20 11:58:09 -0700

    Remove empty test scaffolds

--- src/components/basics/CodeSnippets/utils/parse-snippet-content.utilts.test.ts
+++
@@ -1,7 +0,0 @@
-import { trimSnippet, parseSnippetContent } from './parse-snippet-content.utils';
-
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip('UTILITIES: parse-snippet-content.utils', () => {
-  describe('UTIL: trimSnippet', () => {});
-  describe('UTIL: parseSnippetContent', () => {});
-});
