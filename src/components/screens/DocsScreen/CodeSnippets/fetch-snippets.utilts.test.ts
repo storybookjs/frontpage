8e0d4839b1d4e6859835bfdf6a6eb61e7b9adfde
Kyle Gach <kyle.gach@gmail.com>
2024-03-06 10:21:37 -0700

    Fix svelte native-format snippets
    - Coerce `native-format` type to `html` syntax
    - Ensure they always match the current code language
        - Thus, it renders as a snippet tab alongside the JS/TS snippet tab
    - Further prettify the tab names

--- src/components/screens/DocsScreen/CodeSnippets/fetch-snippets.utilts.test.ts
+++ src/components/screens/DocsScreen/CodeSnippets/fetch-snippets.utilts.test.ts
@@ -17,10 +17,33 @@ describe('UTILITIES: fetch-snippets.utils', () => {
       expect(getSnippetType('some-snippet.npm.ts.mdx')).toBe('npm');
       expect(getSnippetType('some-snippet.yarn.ts.mdx')).toBe('yarn');
       expect(getSnippetType('some-snippet.pnpm.ts.mdx')).toBe('pnpm');
+      expect(getSnippetType('some-snippet.native-format.mdx')).toBe('native-format');
       expect(getSnippetType('some-snippet.whatever.ts.mdx')).toBe('ts');
     });
   });
-  describe('UTIL: getSnippetSyntax', () => {});
-  describe('UTIL: isTerminalSnippetBySyntax', () => {});
+  describe('UTIL: getSnippetSyntax', () => {
+    it('SYNTAX: It should return the correct snippet syntax', () => {
+      expect(getSnippetSyntax('js')).toBe('js');
+      expect(getSnippetSyntax('js-2')).toBe('js');
+      expect(getSnippetSyntax('ts')).toBe('ts');
+      expect(getSnippetSyntax('ts-2')).toBe('ts');
+      expect(getSnippetSyntax('npm')).toBe('sh');
+      expect(getSnippetSyntax('npx')).toBe('sh');
+      expect(getSnippetSyntax('pnpm')).toBe('sh');
+      expect(getSnippetSyntax('yarn')).toBe('sh');
+      expect(getSnippetSyntax('native-format')).toBe('html');
+      expect(getSnippetSyntax('whatever')).toBe('whatever');
+    });
+  });
+  describe('UTIL: isTerminalSnippetByType', () => {
+    it('TERMINAL: It should determine terminal snippets by type', () => {
+      expect(isTerminalSnippetByType('js')).toBe(false);
+      expect(isTerminalSnippetByType('ts')).toBe(false);
+      expect(isTerminalSnippetByType('npm')).toBe(true);
+      expect(isTerminalSnippetByType('npx')).toBe(true);
+      expect(isTerminalSnippetByType('pnpm')).toBe(true);
+      expect(isTerminalSnippetByType('yarn')).toBe(true);
+    });
+  });
   describe('UTIL: fetchDocsSnippets', () => {});
 });
